d38b9841f336f610e1d9c438d8ce59227c254624
victorpinon <victorpinonrattia@hotmail.com>
2019-04-08 20:56:08 +0200

    eliminat el que no calia. Ja funciona tot

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { SubjectsComponent } from './components/subjects/subjects.component';
 import { StationsComponent } from './components/stations/stations.component';
 import { BikesComponent } from './components/bikes/bikes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubjectsComponent,
     StationsComponent,
     BikesComponent
   ],
